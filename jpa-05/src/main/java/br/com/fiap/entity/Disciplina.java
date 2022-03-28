78d9647bcaf9a44d1e8f61186b9afde8a880cfa6
DeltaAlchemist <delta.alchemist@gmail.com>
2022-03-28 12:02:49 -0300

    feat: add curso entity

--- jpa-05/src/main/java/br/com/fiap/entity/Disciplina.java
+++ jpa-05/src/main/java/br/com/fiap/entity/Disciplina.java
@@ -3,13 +3,7 @@ package br.com.fiap.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tb_disciplina")
@@ -49,5 +43,8 @@ public class Disciplina implements Serializable{
 
     @Column(name = "dt_atualizacao")
     private LocalDateTime dataAtualizacao;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Curso curso;
 
 }
