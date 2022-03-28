f3998d6fd3f0f3a7a1c693260aaa0a1f3ea7d042
DeltaAlchemist <delta.alchemist@gmail.com>
2022-03-28 10:44:52 -0300

    feat: add Endereco class

--- jpa-05/src/main/java/br/com/fiap/App.java
+++ jpa-05/src/main/java/br/com/fiap/App.java
@@ -3,7 +3,9 @@ package br.com.fiap;
 import java.time.LocalDate;
 
 import br.com.fiap.entity.Aluno;
+import br.com.fiap.entity.Disciplina;
 import br.com.fiap.service.AlunoServiceImpl;
+import br.com.fiap.service.DisciplinaServiceImpl;
 
 public class App {
 
@@ -15,5 +17,12 @@ public class App {
         alunoService.inserir(new Aluno("Aluno1", "11111", "111.111.111-11", LocalDate.of(1980, 1, 1)));
         alunoService.inserir(new Aluno("Aluno2", "22222", "222.222.222-22", LocalDate.of(1980, 1, 1)));
         alunoService.inserir(aluno3);
+
+        DisciplinaServiceImpl disciplinaService = DisciplinaServiceImpl.getInstance();
+
+        Disciplina disciplina3 = new Disciplina("Matemática", 5000);
+        disciplinaService.inserir(new Disciplina("História", 4000));
+        disciplinaService.inserir(new Disciplina("Física", 3000));
+        disciplinaService.inserir(disciplina3);
     }
 }
