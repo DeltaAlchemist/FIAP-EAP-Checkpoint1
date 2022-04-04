dbea6ae15fa1c6aaa90928fbf59295a794669400
Henrique Campos <50759816+DeltaAlchemist@users.noreply.github.com>
2022-04-04 10:17:55 -0300

    fix: add localdate to test

--- CP1/src/main/java/br/com/fiap/App.java
+++ CP1/src/main/java/br/com/fiap/App.java
@@ -15,19 +15,22 @@ public class App {
                 "O dia que a internet da FIAP funcionou",
                 "Henrique",
                 "978-0-7334-2607-4",
-                2)
+                2,
+                LocalDate.of(1997, 1, 1))
         );
         livroService.inserir(new Livro(
                 "Dias que a internet da FIAP não funcionou",
                 "Benicio",
                 "978-0-7334-2608-4",
-                1)
+                1,
+                LocalDate.of(1993, 4, 4))
         );
         livroService.inserir(new Livro(
                 "Alguém me ajuda",
                 "Douglas Adams",
                 "978-0-7334-2609-4",
-                1)
+                1,
+                LocalDate.of(1990, 1, 1))
         );
 
         List<Livro> livros = livroService.listar();
@@ -42,7 +45,8 @@ public class App {
                 "Dias que a internet da FIAP não funcionou",
                 "Benicio",
                 "978-0-7334-2608-4",
-                3)
+                3, // novo valor de edição
+                LocalDate.of(1997, 1, 1))
         ));
 
     }
